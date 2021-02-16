d4e57ef4c11d8d8114c17241a67085314ad98f10
xuzhuoxi <mailxuzhuoxi@163.com>
2021-02-16 18:11:39 +0800

    1.增加控制台信息打印。 2.增加mac平台下的demo。

--- src/lib/size.go
+++ src/lib/size.go
@@ -1,8 +1,14 @@
 package lib
 
+import "fmt"
+
 type SplitSize struct {
 	//宽
 	Width int
 	//高
 	Height int
+}
+
+func (s SplitSize) String() string {
+	return fmt.Sprintf("{Width=%d, Height=%d}", s.Width, s.Height)
 }
